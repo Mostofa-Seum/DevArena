0ee7a06b06e6aa897cfc57489e1d5e93b92735aa
Seum <mostofaseum8@gmail.com>
2026-05-14 12:15:41 +0600

    Add project files.

---
+++ DevArena.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using DevArena.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevArena.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
